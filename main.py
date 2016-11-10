0d3cf5ce743c9238ef78868b1c12ede70506f45e
Shawn Wang <740616@qq.com>
2016-11-10 20:29:29 +0800

    load data as the timeSeries

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 
 #从数据库中取出数据
 conn = MySQLdb.connect(host='localhost', user='root', db='stock')
-symbol = "sh601919"
-df = pd.read_sql("select * from stockdaykline where symbol = '%s'" % symbol, con=conn, index_col=['date'])
+symbol = "sz300033"
+df = pd.read_sql("select * from stockdaykline where symbol = '%s'" % symbol, con=conn, index_col=['date'], parse_dates=['date'])
 
 #生成时间序列
-ts = pd.Series(df['open'], index=df.index)
-ts = ts[ts.values > 0][-200:]
-
+ts = df.close
+ts = ts[ts.values > 0]
 #绘图
 plt.figure()
 ts.plot(title=df['name'][-1], grid=True)
