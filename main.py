1babd2aaa9089e199f1fb35deaf44bf733d54f25
Shawn Wang <740616@qq.com>
2016-11-10 20:38:15 +0800

    draw 2 images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as nd
+import numpy as np
 import MySQLdb
 import matplotlib.pyplot as plt
 
@@ -7,12 +7,14 @@ import matplotlib.pyplot as plt
 conn = MySQLdb.connect(host='localhost', user='root', db='stock')
 symbol = "sz300033"
 df = pd.read_sql("select * from stockdaykline where symbol = '%s'" % symbol, con=conn, index_col=['date'], parse_dates=['date'])
-
-#生成时间序列
 ts = df.close
-ts = ts[ts.values > 0]
-#绘图
+
+#计算与绘图
 plt.figure()
+ts = ts['2015':'2016']
+ts.plot(title=df['name'][-1], grid=True)
+
+ts = ts.rolling(window=12, center=False).mean()
 ts.plot(title=df['name'][-1], grid=True)
 plt.show()
 
