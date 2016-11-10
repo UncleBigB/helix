93a968f3c40e0eced2b7488e7771e9babae21095
Shawn Wang <740616@qq.com>
2016-11-10 15:48:10 +0800

    fix symbol

--- crawler/quote.py
+++ crawler/quote.py
@@ -6,8 +6,11 @@ import time
 
 def fetchStockList():
     resp = urllib.request.urlopen("http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=0&num=8000&sort=symbol&asc=1&node=hs_a")
-
     data = resp.read().decode('gbk')
+
+    resp = urllib.request.urlopen("http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=0&num=8000&sort=symbol&asc=1&node=hs_s")
+    data = data + resp.read().decode('gbk')
+
     pattern = re.compile(r'\{[^}]+\}')
     stocklist = pattern.findall(data)
 
@@ -16,8 +19,9 @@ def fetchStockList():
 
     for x in stocklist:
         json = demjson.decode(x)
+
         cur = db.cursor()
-        cur.execute("""REPLACE INTO stocklists (symbol, code, name, type) VALUES (%s, %s, %s, '')""", (json['symbol'], json['code'], json['name']))
+        cur.execute("""REPLACE INTO stocklists (symbol, code, name, type) VALUES (%s, %s, %s, %s)""", (json['symbol'], json['code'], json['name'], json['symbol'][0:2]))
 
     db.commit()
 
@@ -30,14 +34,14 @@ def fetchDayKline():
     l = cur.fetchall()
     for s in l:
         url = "http://quotes.money.163.com/service/chddata.html?code="
-        if s[0].startswith("sz"):
+        if s[3] == "sz":
             url = url + "1" + s[1]
-        elif s[0].startswith("sh"):
+        elif s[3] == "sh":
             url = url + "0" + s[1]
         else:
             pass
 
-        url = url + "&start=" + today
+        # url = url + "&start=" + today
 
         print(url)
         resp = urllib.request.urlopen(url)
@@ -47,12 +51,11 @@ def fetchDayKline():
             l = l.replace("None", "0")
             c = l.split(",")
             if len(c) == 16:
-                cur.execute("REPLACE INTO stockdaykline (date, symbol, name, close, high, low, open, yestclose, updatevol, updatepercent, turnoverrate, volume, turnover, marketcap, circulation, quantity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14], c[15]))
+                for k, v in enumerate(c):
+                    if v == "":
+                        c[k] = 0
+                cur.execute("REPLACE INTO stockdaykline (date, code, symbol, name, close, high, low, open, yestclose, updatevol, updatepercent, turnoverrate, volume, turnover, marketcap, circulation, quantity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (c[0], c[1][1:], s[0], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14], c[15]))
         db.commit()
-
-    cur.execute("UPDATE stockdaykline SET symbol = TRIM(LEADING \"'\" FROM symbol)")
-    db.commit()
-
 
 
 
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 #从数据库中取出数据
 conn = MySQLdb.connect(host='localhost', user='root', db='stock')
-symbol = "601398"
+symbol = "sh601919"
 df = pd.read_sql("select * from stockdaykline where symbol = '%s'" % symbol, con=conn, index_col=['date'])
 
 #生成时间序列
 ts = pd.Series(df['open'], index=df.index)
-ts = ts[ts.values > 0]
+ts = ts[ts.values > 0][-200:]
 
 #绘图
 plt.figure()
